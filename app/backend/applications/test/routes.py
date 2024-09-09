f3a68ce79471864e9a5d708f222966e5af28526c
MrLening1 <mister.lening@yandex.ru>
2024-09-09 17:44:46 +0300

    add comments & fix code

--- app/backend/applications/test/routes.py
+++ app/backend/applications/test/routes.py
@@ -17,7 +17,16 @@ router = APIRouter()
 async def write_data(
     body: WriteUpdateSchema,
     redis: aioredis.Redis = Depends(get_redis)
-):
+) -> JSONResponse:
+    """Add an entry
+
+    Args:
+        body (WriteUpdateSchema): The main body of the request
+        redis (aioredis.Redis, optional): Object redis. Defaults to Depends(get_redis).
+
+    Returns:
+        JSONResponse: result write
+    """
     result: Optional[bool] = await redis.setex(name=body.phone, value=body.address, time=Settings.default_time)
     return JSONResponse(content={"result": result}, status_code=status.HTTP_201_CREATED)
 
@@ -26,14 +35,39 @@ async def write_data(
 async def write_data(
     body: WriteUpdateSchema,
     redis: aioredis.Redis = Depends(get_redis)
-):
-    result: Optional[bool] = await redis.setex(name=body.phone, value=body.address, time=Settings.default_time)
-    return JSONResponse(content={"result": result}, status_code=status.HTTP_202_ACCEPTED)
+) -> JSONResponse:
+    """Update the address by phone
+
+    Args:
+        body (WriteUpdateSchema): The main body of the request
+        redis (aioredis.Redis, optional): Object redis. Defaults to Depends(get_redis).
+
+    Returns:
+        JSONResponse: result update
+    """
+    find: Optional[str] = await redis.get(name=body.phone)
+    if isinstance(find, str):
+        result: Optional[bool] = await redis.setex(name=body.phone, value=body.address, time=Settings.default_time)
+        return JSONResponse(content={"result": result}, status_code=status.HTTP_202_ACCEPTED)
+
+    return JSONResponse(content={"result": None}, status_code=status.HTTP_404_NOT_FOUND)
 
 @router.get("/check_data")
 async def check_data(
     phone: str,
     redis: aioredis.Redis = Depends(get_redis)
-):
-    result: ResponseT = await redis.get(name=phone)
-    return JSONResponse(content={"result": result}, status_code=status.HTTP_200_OK)
+) -> JSONResponse:
+    """Get the address by phone
+
+    Args:
+        phone (str): number phone
+        redis (aioredis.Redis, optional): Object redis. Defaults to Depends(get_redis).
+
+    Returns:
+        JSONResponse: result data (address)
+    """
+    result: Optional[str] = await redis.get(name=phone)
+    if isinstance(result, str):
+        return JSONResponse(content={"result": result}, status_code=status.HTTP_200_OK)
+
+    return JSONResponse(content={"result": None}, status_code=status.HTTP_404_NOT_FOUND)
